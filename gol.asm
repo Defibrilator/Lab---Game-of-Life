e19fbee75df8263bd6e68b39f50f026fde835d97
rfluecki <raphael.fluckiger@epfl.ch>
2020-12-01 19:51:05 +0100

    corrected decrement_step for t2 to be set

--- gol.asm
+++ gol.asm
@@ -696,9 +696,9 @@ main:
     ; BEGIN:decrement_step
 	decrement_step: 
 		ldw t0, PAUSE(zero)						#t0 = PAUSE
+		ldw t2, CURR_STEP(zero)					#t2 = CURR_STEP
 		beq t0, zero, ds_ret_zero				#else if pause == 0 -> display and return 0
 		ldw t1, CURR_STATE(zero)				#t1 = CURR_STATE
-		ldw t2, CURR_STEP(zero)					#t2 = CURR_STEP
 		cmpeqi t1, t1, RUN						#t1 = curr_state == run
 		beq t1, zero, ds_ret_zero				#if t1 = false -> display and return 0
 		beq t2, zero, ds_ret_one				#if curr_step == 0 -> return 1
